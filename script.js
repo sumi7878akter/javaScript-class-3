e5d74ae3f3d0c63a096030b244c560dd58ea4a75
sumi7878akter <jannatulislamsumi0@gmail.com>
2024-02-20 14:35:50 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+
+
+var mach = 60;
+var good rice =true
+var rice = 20;
+var dal =10;
+var myBudgut=30
+
+
+
+
+
+
+if( mach < myBudgut  )
+    
+if(' good rice == false'){
+        console.log ('mach ami khabo na');
+  
+    }else {
+
+    console.log('tur kopale vat mach diya vat khabo');
+}
+
+else if(rice<=myBudgut&&
+    console.log(rice and dal,  ))
